ab67dc4e873e3c47f1bfd8fbe229572f67668c69
dilyanatanasov <dilyanatanasov177@gmail.com>
2022-02-10 19:11:44 +0200

    Uploading basic js

---
+++ index.js
@@ -0,0 +1,44 @@
+let age = 26;
+// console.log(age);
+age = 27;
+// console.log(age);
+
+const ip_address_server = "192.168.0.1";
+// console.log(ip_address_server);
+
+let boolean = true // false
+
+const array = [
+    age,
+    ip_address_server,
+    boolean
+]
+
+// for (let i = 0; i < array.length; i++) {
+//     console.log(array[i]);
+// }
+
+// let i = 0;
+// while (i < array.length) {
+//     console.log(array[i]);
+//     i++;
+// }
+
+// for (let element of array) {
+//     console.log(element)
+// }
+
+const object = {
+    age: "33",
+    ip_address_server: "192.168.0.11"
+}
+// console.log(object)
+
+function func() {
+    return "Success";
+}
+
+const isOdd = () => {
+    return "Success";
+}
+console.log(isOdd());
